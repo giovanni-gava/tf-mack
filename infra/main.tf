79e23501aeb213c5a9196c0ea4cefccd9470ab51
Carol Gava <carolgava@MacBook-Air-de-Carol.local>
2024-09-27 22:54:16 -0300

    feat(): fix terraform init step on terraform.yaml

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,13 @@
 module "ec2" {
-    source = "../modules/ec2"
+    source = "./modules/ec2"
     region = "us-east-1"
-    vpc_id = "vpc-0162a2811ed1ab339"
-    instance_name = "ec2-test"
-    instance_type = "t2.micro"
-    volume_name = "disco-1"
-    volume_size = 20
-    sg_name = "meu-sg"
-    key_name = "terraform.pem"
+    vpc_id = "vpc-063ad540e9672c793"
+    instance_name = var.instance_name
+    instance_type = var.instance_type 
+    volume_name = var.volume_name
+    volume_size = var.volume_size
+    sg_name = var.sg_name
+    key_name = var.key_pair
     allowed_https_cidr_blocks = [ "0.0.0.0/0" ]
     allowed_ssh_cidr_blocks = [ "0.0.0.0/0" ]
 }
